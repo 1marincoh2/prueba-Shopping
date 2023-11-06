6659ddfe84a02cec1ced3952236ed8463faf26b0
1marincoh2 <josealbertomarincoh1@gmail.com>
2023-11-05 23:42:05 -0500

    fix:life toast

--- src/hook/useToast.ts
+++ src/hook/useToast.ts
@@ -4,7 +4,7 @@ import { useToast as useNotify } from "primevue/usetoast";
 export function useToast(){
     const toast = useNotify();
     const show = (severity: ToastMessageOptions["severity"], summary: string, detail: string) => {
-        toast.add({ severity: severity, summary: summary, detail: detail, life: 5000 });
+        toast.add({ severity: severity, summary: summary, detail: detail, life: 3000 });
     };
     return {show}
 }
